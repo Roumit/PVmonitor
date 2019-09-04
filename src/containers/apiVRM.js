f8dd45a16c08223f89174e8636235296057cbf98
Timur Bilotserkivky <timur.belotserkovsky@gmail.com>
2019-09-05 00:56:17 +0300

    small fix

--- src/containers/apiVRM.js
+++ src/containers/apiVRM.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { setToken } from "../reducers/loginVRM";
 import { setInstallations } from "../reducers/installationsVRM";
 import { setInstallationObjectData } from "../reducers/installationsObjectData";
